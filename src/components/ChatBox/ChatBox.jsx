afa94dc50ac5bbb1e82f2f26238af1e4547e2022
KaustubhKT02 <kaustubhtripathi36@gmail.com>
2026-02-01 13:59:50 +0530

    feat: Add chat message display and input functionality to ChatBox component

--- src/components/ChatBox/ChatBox.jsx
+++ src/components/ChatBox/ChatBox.jsx
@@ -18,7 +18,81 @@ function ChatBox() {
             alt="green-dot icon"
           />
         </p>
-        <img src={assets.help_icon} alt="help icon" />
+        <img src={assets.help_icon} className="max-w-6" alt="help icon" />
+      </div>
+      {/* Chat message */}
+      <div className="h-[calc(100%-70px)] pb-12.5 overflow-y-scroll flex flex-col-reverse">
+        <div className="flex items-end  flex-row-reverse justify-start gap-1.25 py-0 px-3.75">
+          <p className="text-white bg-[#077EFF] p-4 max-w-50 text-[11px] font-light rounded-t-lg rounded-b-lg rounded-l-lg  mb-7.5">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Repudiandae doloremque molestias ea dignissimos.
+          </p>
+          <div>
+            <img
+              src={assets.profile_img}
+              className="w-6.75 aspect-square rounded-[50px] "
+              alt=""
+            />
+            <p className="text-center text-[9px] ">2:30pm</p>
+          </div>
+        </div>
+        <div className="flex items-end  flex-row- justify-end gap-1.25 py-0 px-3.75">
+          <img
+            src={assets.pic1}
+            className="max-w-57.5 mb-7.5 rounded-[10px]"
+            alt=""
+          />
+          <div>
+            <img
+              src={assets.profile_img}
+              className="w-6.75 aspect-square rounded-[50px] "
+              alt=""
+            />
+            <p className="text-center text-[9px] ">2:30pm</p>
+          </div>
+        </div>
+        <div className="flex items-end gap-1.25 py-0 px-3.75 flex-row-reverse justify-end">
+          <p className="text-white bg-[#077EFF] p-4 max-w-50 text-[11px] font-light rounded-t-lg rounded-b-lg rounded-l-lg  mb-7.5">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Repudiandae doloremque molestias ea dignissimos.
+          </p>
+          <div>
+            <img
+              src={assets.profile_img}
+              className="w-6.75 aspect-square rounded-[50px] "
+              alt=""
+            />
+            <p className="text-center text-[9px] ">2:30pm</p>
+          </div>
+        </div>
+      </div>
+
+      {/* Chat Input */}
+      <div className="flex items-center gap-5 py-2.5 px-3.5 bg-white absolute bottom-0 left-0 right-0">
+        <input
+          type="text"
+          className="flex-1 border-0 outline-0"
+          placeholder="Send a message"
+        />
+        <input
+          type="file"
+          className="cursor-pointer w-7.5"
+          id="image"
+          accept="image/png, image/jpeg"
+          hidden
+        />
+        <label className="flex " htmlFor="image">
+          <img
+            src={assets.gallery_icon}
+            className="cursor-pointer w-5.5"
+            alt="gallery_icon"
+          />
+        </label>
+        <img
+          src={assets.send_button}
+          className="cursor-pointer w-7.5"
+          alt="Send button icon"
+        />
       </div>
     </div>
   );
