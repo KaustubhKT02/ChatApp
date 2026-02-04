d54c083001a55107bc465c22b386ab412e72c688
KaustubhKT02 <kaustubhtripathi36@gmail.com>
2026-02-04 13:50:27 +0530

    feat: Configure Firebase with environment variables for secure API keys

--- src/config/firebase.config.js
+++ src/config/firebase.config.js
@@ -2,14 +2,15 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBOrwtmCC_dZz9z8mcCoIWqyeZvR8n_2wg",
-  authDomain: "chat-app-1a771.firebaseapp.com",
-  projectId: "chat-app-1a771",
-  storageBucket: "chat-app-1a771.firebasestorage.app",
-  messagingSenderId: "910563451741",
-  appId: "1:910563451741:web:fe6976d1b901e2bf073eb4",
-  measurementId: "G-6B4064W03Y"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN, 
+  projectId: import.meta.env.VITE_PROJECTID, 
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET, 
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID, 
+  appId: import.meta.env.VITE_APPID, 
+  measurementId: import.meta.env.VITE_MEASUREMENTID
 };
 
 // Initialize Firebase
